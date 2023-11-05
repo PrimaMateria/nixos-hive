89101b0e8ce3a1b37619224e258d200d40e553e2
matus.benko <matus.benko@gmail.com>
2023-11-05 14:51:59 +0100

    reduce complexity in flake

--- cells/primamateria/bee.nix
+++ cells/primamateria/bee.nix
@@ -9,7 +9,7 @@
 #        ████░░██░░░░██  
 #          ██████████
 
-{ inputs }: {
+{ inputs, cell }: {
   system = "x86_64-linux";
   pkgs = inputs.nixpkgs;
   home = inputs.home-manager;
--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-# flake.nix
 {
   description = "PrimaMateria's NixOS configuration";
 
@@ -22,26 +21,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixago = {
-      url = "github:nix-community/nixago";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     std = {
       url = "github:divnix/std";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        arion.follows = "arion";
-        devshell.follows = "devshell";
-        devshell.inputs.nixpkgs.follows = "nixpkgs";
-        nixago.follows = "nixago";
       };
     };
 
@@ -49,41 +32,11 @@
       url = "github:divnix/hive";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-        disko.follows = "disko";
-        colmena.follows = "colmena";
-        nixos-generators.follows = "nixos-generators";
       };
-    };
-
-    colmena = {
-      url = "https://flakehub.com/f/zhaofengli/colmena/0.4.0.tar.gz";
-      inputs.flake-compat.follows = "";
-    };
-
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators/1.7.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixlib.follows = "nixpkgs";
-    };
-
-    arion = {
-      url = "github:hercules-ci/arion";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    disko = {
-      url =
-        "github:nix-community/disko/5d9f362aecd7a4c2e8a3bf2afddb49051988cab9";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, hive, std, ... }@inputs:
-    let
-      # lib = inputs.nixpkgs.lib // buitlins;
-      lib = inputs.nixpkgs.lib;
-    in
     hive.growOn
       {
         inherit inputs;
@@ -106,33 +59,9 @@
 
           # configurations
           nixosConfigurations
-          # diskoConfigurations
-          # colmenaConfigurations
-          # (installables "generators")
-          # (installables "installers")
-
-
-          # pkgs
-          # (pkgs "pkgs")
-
-          # devshells
-          # (nixago "configs")
-          # (devshells "devshells")
         ];
       }
       {
-        # devShells = std.harvest self [ "repo" "devshells" ];
-        # packages =
-        #   let
-        #     # generators = std.harvest self [ "repo" "generators" ];
-        #     # installers = std.harvest self [ "primamateria" "installers" ];
-        #   in
-        #   {
-        #     inherit (installers) x86_64-linux;
-        #   };
-      }
-      {
         nixosConfigurations = hive.collect self "nixosConfigurations";
-        # colmenaHive = collect self "colmenaConfigurations";
       };
 }
