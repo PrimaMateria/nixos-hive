d7ae3de7f31c753c3efb897abc594b511b9c1d1f
matus.benko <matus.benko@gmail.com>
2023-11-25 18:51:41 +0100

    clean

--- cells/primamateria/system/vnc.nix
+++ cells/primamateria/system/vnc.nix
@@ -3,20 +3,10 @@ let
   inherit (inputs) nixpkgs;
   inherit (cell) secrets;
 
-  # vncXstartup = nixpkgs.writeShellApplication {
-  #   name = "xstartup";
-  #   runtimeInputs = with nixpkgs; [ icewm ];
-  #   text = "icewm";
-  # };
-
   vncPasswd = nixpkgs.writeTextFile {
     name = "vnc-passwd";
     text = secrets.vncPassword;
   };
-
-  # programs.bash.shellAliases = {
-  #   runx = "xinit ${vncXstartup}/bin/xstartup -- $(realpath $(which Xvnc)) :1 PasswordFile=${vncPasswd}";
-  # };
 
   runx = nixpkgs.writeShellApplication {
     name = "runx";
