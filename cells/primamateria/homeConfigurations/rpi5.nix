726743d1ee4ec954195d3247eb5be24ab4e16610
matus.benko <matus.benko@gmail.com>
2024-06-09 18:53:49 +0200

    leds off first try

--- cells/primamateria/homeConfigurations/rpi5.nix
+++ cells/primamateria/homeConfigurations/rpi5.nix
@@ -5,6 +5,67 @@
   inherit (inputs) nixpkgs;
   inherit (nixpkgs) lib;
   inherit (cell) bees cli;
+
+  boot-firmware-config = nixpkgs.writeText "boot-firmware-config.txt" ''
+    # For more options and information see
+    # http://rptl.io/configtxt
+    # Some settings may impact device functionality. See link above for details
+
+    # include /home/primamateria/.config/rpi-boot-extract-config.txt
+
+    dtparam=pwr_led_trigger=default-on
+    dtparam=pwr_led_activelow=off
+    dtparam=act_led_trigger=none
+    dtparam=act_led_activelow=off
+    dtparam=eth_led0=4
+    dtparam=eth_led1=4
+
+
+    # Uncomment some or all of these to enable the optional hardware interfaces
+    #dtparam=i2c_arm=on
+    #dtparam=i2s=on
+    #dtparam=spi=on
+
+    # Enable audio (loads snd_bcm2835)
+    dtparam=audio=on
+
+    # Additional overlays and parameters are documented
+    # /boot/firmware/overlays/README
+
+    # Automatically load overlays for detected cameras
+    camera_auto_detect=1
+
+    # Automatically load overlays for detected DSI displays
+    display_auto_detect=1
+
+    # Automatically load initramfs files, if found
+    auto_initramfs=1
+
+    # Enable DRM VC4 V3D driver
+    dtoverlay=vc4-kms-v3d
+    max_framebuffers=2
+
+    # Don't have the firmware create an initial video= setting in cmdline.txt.
+    # Use the kernel's default instead.
+    disable_fw_kms_setup=1
+
+    # Run in 64-bit mode
+    arm_64bit=1
+
+    # Disable compensation for displays with overscan
+    disable_overscan=1
+
+    # Run as fast as firmware / board allows
+    arm_boost=1
+
+    [cm4]
+    # Enable host mode on the 2711 built-in XHCI USB controller.
+    # This line should be removed if the legacy DWC2 controller is required
+    # (e.g. for USB device mode) or if USB support is not required.
+    otg_mode=1
+
+    [all]
+  '';
 in {
   bee = bees.rpi;
   imports = [
@@ -18,6 +79,19 @@ in {
         homeDirectory = "/home/primamateria";
         stateVersion = "22.05";
       };
+
+      home.packages = [
+        (nixpkgs.writeShellApplication
+          {
+            name = "rpi5-configure";
+            text = ''
+              # Backup original /boot/firmware/config.txt
+              sudo cp /boot/firmware/config.txt /boot/firmware/config.backup.txt
+              # Copy custom config from the nix store
+              sudo cp ${boot-firmware-config} /boot/firmware/config.txt
+            '';
+          })
+      ];
     }
   ];
 }
