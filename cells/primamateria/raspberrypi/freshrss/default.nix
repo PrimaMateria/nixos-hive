6047f8ed490fb2b1c83c95b87fef84d0a8b8d8f9
matus.benko <matus.benko@gmail.com>
2024-06-17 01:49:49 +0200

    https

--- cells/primamateria/raspberrypi/freshrss/default.nix
+++ cells/primamateria/raspberrypi/freshrss/default.nix
@@ -16,8 +16,18 @@
       services = with secrets.freshrssServer; {
         freshrss = {
           labels = [
+            "traefik.enable=true"
+            "traefik.http.middlewares.freshrssM1.compress=true"
+            "traefik.http.middlewares.freshrssM2.headers.browserXssFilter=true"
+            "traefik.http.middlewares.freshrssM2.headers.forceSTSHeader=true"
+            "traefik.http.middlewares.freshrssM2.headers.frameDeny=true"
+            "traefik.http.middlewares.freshrssM2.headers.referrerPolicy=no-referrer-when-downgrade"
+            "traefik.http.middlewares.freshrssM2.headers.stsSeconds=31536000"
+            "traefik.http.routers.freshrss.entrypoints=https"
+            "traefik.http.routers.freshrss.tls.certResolver=letsEncrypt"
+            "traefik.http.routers.freshrss.tls=true"
             "traefik.http.middlewares.freshrssM3.stripprefix.prefixes=/freshrss"
-            "traefik.http.routers.freshrss.middlewares=freshrssM3"
+            "traefik.http.routers.freshrss.middlewares=freshrssM1,freshrssM2,freshrssM3"
             "traefik.http.routers.freshrss.rule=PathPrefix(`/freshrss`)"
           ];
           image = "freshrss/freshrss";
--- cells/primamateria/raspberrypi/traefik/default.nix
+++ cells/primamateria/raspberrypi/traefik/default.nix
@@ -5,21 +5,65 @@
   inherit (cell) secrets;
   inherit (inputs) nixpkgs;
 
+  tls = nixpkgs.writeTextFile {
+    name = "traefik-tls.yaml";
+    text = builtins.toJSON {
+      options.default = {
+        minVersion = "VersionTLS12";
+        sniStrict = true;
+        cipherSuites = [
+          "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+          "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+          "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+          "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+          "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
+          "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
+          "TLS_AES_128_GCM_SHA256"
+          "TLS_AES_256_GCM_SHA384"
+          "TLS_CHACHA20_POLY1305_SHA256"
+        ];
+      };
+    };
+  };
+
   dockerCompose = nixpkgs.writeTextFile {
     name = "traefik-docker-compose.yaml";
     text = builtins.toJSON {
-      version = "3";
+      version = "2.4";
+      volumes = {
+        traefik-letsencrypt = null;
+        traefik-tmp = null;
+      };
       services = {
         traefik = {
           image = "traefik:v3.0";
+          container_name = "traefik";
           network_mode = "host";
-          command = "--api.insecure=true --providers.docker";
           ports = [
             "80:80"
             "8080:8080"
+            "443:443"
           ];
           volumes = [
             "/var/run/docker.sock:/var/run/docker.sock"
+            "traefik-tmp:/tmp"
+            "traefik-letsencrypt:/etc/traefik/acme"
+            "${tls}:/etc/traefik/tls.yaml:ro"
+          ];
+          command = [
+            "--api.insecure=true"
+            "--providers.docker=true"
+            "--entryPoints.http.address=:80"
+            "--entryPoints.https.address=:443"
+            "--entryPoints.http.http.redirections.entryPoint.to=https"
+            "--entryPoints.http.http.redirections.entryPoint.scheme=https"
+            "--certificatesResolvers.letsEncrypt.acme.storage=/etc/traefik/acme/acme.json"
+            "--certificatesResolvers.letsEncrypt.acme.email=matus.benko@gmail.com"
+            "--certificatesResolvers.letsEncrypt.acme.tlsChallenge=true"
+            "--providers.file.filename=/etc/traefik/tls.yaml"
+          ];
+          labels = [
+            "traefik.enable=false"
           ];
         };
       };
