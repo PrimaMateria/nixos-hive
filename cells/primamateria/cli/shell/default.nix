4eab5e72f1e784af975951a63f2d1588fa182f1e
matus.benko <matus.benko@gmail.com>
2024-12-15 20:47:09 +0100

    nvim aliases

--- cells/primamateria/cli/shell/default.nix
+++ cells/primamateria/cli/shell/default.nix
@@ -23,7 +23,9 @@ in {
         ls = nixpkgs.lib.mkForce "eza --time-style long-iso";
         cat = "bat -p";
         top = "btop";
-        nvim = "nix run ~/dev/neovim-nix --";
+        nvim = "nix run ~/dev/neovim-nix#neovim.base --";
+        nvim-web = "nix run ~/dev/neovim-nix#neovim.web --";
+        nvim-puml = "nix run ~/dev/neovim-nix#neovim.puml --";
       };
     };
   };
