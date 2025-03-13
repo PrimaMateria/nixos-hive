0f74acc84f84ad64e778bbc85f85e6edb6024062
matus.benko <matus.benko@gmail.com>
2025-03-13 10:26:29 +0100

    remove borked service repo init

--- cells/primamateria/environments/clicraft.nix
+++ cells/primamateria/environments/clicraft.nix
@@ -12,47 +12,9 @@ in {
     cli.feeds
     cli.ambients
     cli.vifm
-    cli.dev
     cli.weechat
 
     {
-      primamateria.cli.dev = {
-        projects = [
-          {
-            name = "nixos-hive";
-            url = "git@github.com:PrimaMateria/nixos-hive.git";
-          }
-          {
-            name = "experiment-haumea";
-            url = "git@github.com:PrimaMateria/experiment-haumea.git";
-          }
-          {
-            name = "experiment-paisano";
-            url = "git@github.com:PrimaMateria/experiment-paisano.git";
-          }
-          {
-            name = "startpages";
-            url = "git@github.com:PrimaMateria/startpages.git";
-          }
-          {
-            name = "blog";
-            url = "git@github.com:PrimaMateria/blog.git";
-          }
-          {
-            name = "nixos";
-            url = "git@github.com:PrimaMateria/nixos.git";
-          }
-          {
-            name = "neovim-nix";
-            url = "git@github.com:PrimaMateria/neovim-nix.git";
-          }
-          {
-            name = "qmk_fjrmware";
-            url = "git@github.com:PrimaMateria/qmk_firmware.git";
-          }
-        ];
-      };
-
       primamateria.cli.tmux = {
         sessions = [
           {
--- cells/primamateria/homeConfigurations/wokwok.nix
+++ cells/primamateria/homeConfigurations/wokwok.nix
@@ -17,7 +17,6 @@ in {
     cli.feeds
     cli.ambients
     cli.vifm
-    cli.dev
     # cli.weechat
 
     {
@@ -25,71 +24,6 @@ in {
         username = "primamateria";
         homeDirectory = "/home/primamateria";
         stateVersion = "22.05";
-      };
-
-      primamateria.cli.dev = {
-        projects = [
-          {
-            name = "nixos-hive";
-            url = "git@github.com:PrimaMateria/nixos-hive.git";
-          }
-          {
-            name = "startpages";
-            url = "git@github.com:PrimaMateria/startpages.git";
-          }
-          {
-            name = "blog";
-            url = "git@github.com:PrimaMateria/blog.git";
-          }
-          {
-            name = "nixos";
-            url = "git@github.com:PrimaMateria/nixos.git";
-          }
-          {
-            name = "neovim-nix";
-            url = "git@github.com:PrimaMateria/neovim-nix.git";
-          }
-          {
-            name = "finapi-build-library";
-            url = "git@bitbucket.org:teaminvest/build-library.git";
-          }
-          {
-            name = "finapi-cms";
-            url = "git@bitbucket.org:teaminvest/cms.git";
-          }
-          {
-            name = "finapi-design-system";
-            url = "git@bitbucket.org:teaminvest/design-system.git";
-          }
-          {
-            name = "finapi-hostpages";
-            url = "git@bitbucket.org:teaminvest/hostpages.git";
-          }
-          {
-            name = "finapi-process-ctrl";
-            url = "git@bitbucket.org:teaminvest/process-ctrl.git";
-          }
-          {
-            name = "finapi-widget-library";
-            url = "git@bitbucket.org:teaminvest/widget-library.git";
-          }
-          {
-            name = "finapi-js-loader";
-            url = "git@bitbucket.org:teaminvest/js-loader.git";
-          }
-          {
-            name = "finapi-js-static-resources";
-            url = "git@bitbucket.org:teaminvest/js-static-resources.git";
-          }
-          {
-            name = "web-form";
-            url = "git@bitbucket.org:teaminvest/web-form.git";
-          }
-          {
-            name = "web-form-loader";
-            url = "git@bitbucket.org:teaminvest/web-form-loader.git";
-          }
-        ];
       };
 
       primamateria.cli.tmux = {
