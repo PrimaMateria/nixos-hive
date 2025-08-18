e5a6fbb05c774e647e389e8bfd1fe7b2f0c8104f
matus.benko <matus.benko@gmail.com>
2025-08-18 10:21:33 +0200

    update

--- cells/primamateria/system/vnc.nix
+++ cells/primamateria/system/vnc.nix
@@ -24,7 +24,7 @@ in {
   config = {
     services.xserver.autorun = false;
     services.xserver.windowManager.icewm.enable = true;
-    services.xserver.displayManager.gdm.enable = true;
+    services.displayManager.gdm.enable = true;
 
     environment.systemPackages = [runx];
   };
