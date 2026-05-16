3ee5d5876f3bea6726c0df3030f0ae888f1ff8cd
matus.benko <matus.benko@gmail.com>
2026-05-17 00:58:17 +0200

    stuff

--- cells/primamateria/cli/shellMin/__modules/readline.nix
+++ cells/primamateria/cli/shellMin/__modules/readline.nix
@@ -8,8 +8,8 @@
       set completion-ignore-case on
       set editing-mode vi
       set show-mode-in-prompt on
-      set vi-ins-mode-string " "
-      set vi-cmd-mode-string " "
+      set vi-ins-mode-string "i:"
+      set vi-cmd-mode-string "n:"
     '';
   };
 }
